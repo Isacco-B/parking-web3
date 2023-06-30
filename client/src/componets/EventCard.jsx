6ea1dc0afd87e8d77c0240e947504a6cda7a0676
Isacco-B <git@isaccobertoli.com>
2023-06-30 12:29:13 +0200

    edit event style

--- client/src/componets/EventCard.jsx
+++ client/src/componets/EventCard.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import CustomButton from "./CustomButton";
 
-function EventCard({ eventName, handleClick }) {
+function EventCard({ eventName }) {
   return (
-    <div>
-      <div className="flex flex-row bg-white h-[69px] shadow-md rounded-md justify-between items-center p-4">
-        <div className="p-2 rounded-[10px]">
-          <p className="font-epilogue font-semibold text-[13px] text-black uppercase">
-            {eventName}
-          </p>
-        </div>
-        <CustomButton
-          btnType="button"
-          title={"View"}
-          styles={"bg-[#4BB3FD] "}
-          handleClick={handleClick}
-        />
-      </div>
+    <div className="bg-white h-[69px] shadow-md rounded-md p-4 min-w-[280px] flex items-center justify-center">
+      <p className="font-epilogue font-semibold text-[13px] text-black uppercase">
+        {eventName}
+      </p>
     </div>
   );
 }
--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,51 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { useStateContext } from "../context";
-import { EventCard, Modal } from "../componets";
+import { EventCard } from "../componets";
 import { loader } from "../assets";
-
 
 function Events() {
   const [events, setEvents] = useState([]);
   const { contract, address } = useStateContext();
-  const [isLoading, setIsLoading] = useState(false)
-  const [showModal, setShowModal] = useState(false);
-
-function handleClick() {
-  setShowModal(true)
-}
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function getAllEvents() {
-      setIsLoading(true)
+      setIsLoading(true);
       const allEvents = await contract.events.getAllEvents();
-      console.log(allEvents)
+      console.log(allEvents);
       setEvents(allEvents);
-      setIsLoading(false)
+      setIsLoading(false);
     }
     getAllEvents();
   }, [contract, address]);
 
   return (
-    <div>
-      <Modal showModal={showModal} setShowModal={setShowModal}/>
-      <div className="flex flex-col w-full gap-4">
-        {isLoading && (
-          <div className="flex justify-center w-full h-[300px] bg-white items-center">
-            <img
-              src={loader}
-              alt="loader"
-              className="w-[100px] h-[100px] object-contain"
-            />
-          </div>
-        )}
-        <p className="font-epilogue font-semibold uppercase">Events</p>
-        {events.map((event, i) => (
-          <EventCard
-            key={i}
-            {...event}
-            handleClick={handleClick}
+    <div className="w-full flex flex-col items-center justify-center">
+      <p className="font-epilogue font-semibold text-[28px]">Events</p>
+      {isLoading && (
+        <div className="flex justify-center items-center m-[20px]">
+          <img
+            src={loader}
+            alt="loader"
+            className="w-[100px] h-[100px] object-contain"
           />
-        ))}
+        </div>
+      )}
+      <div className="flex items-center max-w-[800px] mt-[20px]">
+        <div className="flex flex-wrap w-full gap-4 justify-center">
+          {events.map((event, i) => (
+            <EventCard key={i} {...event} />
+          ))}
+        </div>
       </div>
     </div>
   );
